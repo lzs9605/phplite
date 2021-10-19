81eb758f75227e49eb5d433b7576320e74578df7
dreamboymt <lzs9605@gmail.com>
2021-10-19 21:07:44 +0800

    Init commit

---
+++ config.inc.php
@@ -0,0 +1,27 @@
+<?php
+/*
+	[UENEN TECHNOLOGIES] Copyright (c) 2021 unn.tech
+	This is a freeware, use is subject to license.txt
+*/
+
+defined('IN_PHPLite') or exit('Access Denied');
+
+$CFG['database'] = 'mysqli';
+$CFG['pconnect'] = '0';
+$CFG['db_host'] = '';
+$CFG['db_name'] = '';
+$CFG['db_user'] = '';
+$CFG['db_pass'] = '';
+$CFG['db_charset'] = 'utf8';
+$CFG['db_expires'] = '0';
+$CFG['tb_pre'] = '';
+$CFG['cookie_domain'] = '.unn.tech';
+$CFG['cookie_path'] = '/';
+$CFG['cookie_pre'] = 'unn_';
+$CFG['session'] = 'file';
+$CFG['skin'] = 'default';
+$CFG['template'] = 'default';
+$CFG['authkey'] = 'PHPLite_bf6455523d7c1bfbb05bc9c4770293fa';
+
+
+?>
---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '0d0e0672ad16880f1267a1ad7e5940d7d40a23d3',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '0d0e0672ad16880f1267a1ad7e5940d7d40a23d3',
+    ),
+  ),
+);
